79c4978dca5fb4ea73c1136675af76264775fdc3
9900K <46859140+9900K@users.noreply.github.com>
2019-04-03 17:11:33 -0600

    Update martingale.js

--- martingale.js
+++ martingale.js
@@ -1,7 +1,7 @@
 var config = {
   baseBet: { label: "Base bet", type: "balance", value: 100 },             // how many satoshis to bet initially
-  target: { label: "Target", type: "multiplier", value: 2 },               // target multiplier
-  betMultiplier: { label: "Bet multiplier", type: "multiplier", value: 2 } // what to multiply the bet size by when we lose a wager
+  target: { label: "Target", type: "multiplier", value: 11 },               // target multiplier
+  betMultiplier: { label: "Bet multiplier", type: "multiplier", value: 1.15 } // what to multiply the bet size by when we lose a wager
 }
 
 
@@ -12,6 +12,7 @@ while (true) {
   // make the bet and wait for the result
   const { multiplier } = await this.bet(betSize(lossCount), config.target.value)
 
+  
   if (multiplier < config.target.value) { // loss
     lossCount++
     this.log(`Lost bet. Multiplying bet size by ${config.betMultiplier.value} for new bet size of ${betSize(lossCount)} satoshis.`)
@@ -20,6 +21,7 @@ while (true) {
     this.log(`Won bet. Setting bet size to ${config.baseBet.value} satoshis.`)
   }
 }
+    if (`$(this.balance<str.balance)) stop betting ();
 
 function betSize(lossCount) {
   const bet = config.baseBet.value * Math.pow(config.betMultiplier.value, lossCount)
